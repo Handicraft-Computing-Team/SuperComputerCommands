85708796a3566219830017bfe85da211e1f93956
叶璨铭 <12011404@mail.sustech.edu.cn>
2023-01-30 20:08:33 +0800

    feat: conda安装教程

---
+++ software-config/conda.bash
@@ -0,0 +1,13 @@
+# 1. 下载安装脚本
+# 下载链接可以从这两个地方找到
+# 官网： https://www.anaconda.com/products/distribution#Downloads
+# 清华镜像站：https://mirrors.tuna.tsinghua.edu.cn/help/anaconda/
+wget https://mirrors.bfsu.edu.cn/anaconda/archive/Anaconda3-2022.10-Linux-x86_64.sh
+
+# 2. 运行安装脚本
+./Anaconda3-2022.10-Linux-x86_64.sh
+# 3. 集成到shell
+conda init
+
+# 4. 浏览器查看可以安装的包
+https://anaconda.org/anaconda/repo
